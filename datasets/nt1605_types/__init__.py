d4d0544e06edb95762383d0d81186dc06668cc29
Bohdan Starosta <bjstarosta@gmail.com>
2021-02-04 10:34:38 +0000

    adding nt1605_types dataset

---
+++ datasets/nt1605_types/__init__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""nt1605_types dataset.
+
+Generated 03/02/2021.
+Experimental images of AlGaN with marked ground truth, augmented.
+Ground truth discriminates types.
+
+Author: SSD Group, Bohdan Starosta
+University of Strathclyde Physics Department
+"""
+
+import datasets
+
+
+class nt1605_types(datasets.Dataset):
+
+    def __init__(self):
+        super().__init__()
+
+        self.desc = 'Experimental images of AlGaN (15% AlN, Tyndall) w/ ground'
+        + ' truth (incl. TD types)'
+        self.generated = '2021-02-03'
+
+    def load(self, limit=None):
+        if limit is not None and limit < 1:
+            raise datasets.DatasetException('Limit must be at least 1.')
+        # experimental approximation dataset
+        self.X_train = self.load_images_from_dir('exp', limit)
+        # voronoi diagram dataset
+        self.Y_train = self.load_images_from_dir('ground', limit)
---
+++ datasets/nt1605_types/gen.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+OUT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+TOOLS=$( realpath ${OUT}/../tools.py )
+
+CHUNK_W=512
+CHUNK_H=512
+CHUNK_STRIDE=0.25
+PAD_W=128
+PAD_H=128
+
+mkdir -p ${OUT}/exp
+mkdir -p ${OUT}/ground
+
+# augm
+mkdir -p ${OUT}/tmp1a
+mkdir -p ${OUT}/tmp2a
+python ${TOOLS} -v augm \
+  -i ${OUT}/nt1605_img2.png \
+  -o ${OUT}/tmp1a/
+python ${TOOLS} -v augm \
+  -i ${OUT}/nt1605_ground2.png \
+  -o ${OUT}/tmp2a/
+python ${TOOLS} -v augm \
+  -i ${OUT}/nt1605_img3.png \
+  -o ${OUT}/tmp1a/
+python ${TOOLS} -v augm \
+  -i ${OUT}/nt1605_ground3.png \
+  -o ${OUT}/tmp2a/
+
+# split
+mkdir -p ${OUT}/tmp1b
+mkdir -p ${OUT}/tmp2b
+python ${TOOLS} -v split \
+  -i ${OUT}/tmp1a/ \
+  -o ${OUT}/exp/ \
+  --width ${CHUNK_W} --height ${CHUNK_H} --stride ${CHUNK_STRIDE}
+python ${TOOLS} -v split \
+  -i ${OUT}/tmp2a/ \
+  -o ${OUT}/ground/ \
+  --width ${CHUNK_W} --height ${CHUNK_H} --stride ${CHUNK_STRIDE}
+
+# pad
+# python ${TOOLS} -v pad \
+#   -i ${OUT}/tmp1b/ \
+#   -o ${OUT}/exp/ \
+#   --mode symmetric --pad-width ${PAD_W} ${PAD_H}
+# python ${TOOLS} -v pad \
+#   -i ${OUT}/tmp2b/ \
+#   -o ${OUT}/ground/ \
+#   --mode symmetric --pad-width ${PAD_W} ${PAD_H}
+
+# clean up
+rm -r ${OUT}/tmp1a
+rm -r ${OUT}/tmp2a
+rm -r ${OUT}/tmp1b
+rm -r ${OUT}/tmp2b
